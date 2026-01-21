37377c260f25a85afa60c7edf7fa35190ff4820a
zsuzsasz88 <zsuzsasz88@gmail.com>
2026-01-21 14:39:45 +0100

    paraméter beállítások

--- damajatek.py
+++ damajatek.py
@@ -6,3 +6,16 @@
 # - Ütés kötelező, és többszörös ütés is van (ha lehetséges folytatni kell)
 # - Király (B/W): elérve a túloldalt, visszafelé is léphet/üthet
 import os 
+
+tabla_meret = 8
+
+BG_SZURKE = "\033[47m"
+BG_FEKETE = "\033[40m"
+FG_CIANK = "\033[96m"
+FG_SARGA = "\033[93m"
+RESET = "\033[0m"
+EMPTY =  "-"
+FEHER =  FG_CIANK + "w" + RESET
+FEKETE =  FG_SARGA+ "b" + RESET
+FEHER_KIRALY =  FG_CIANK + "W" + RESET
+FEKETE_KIRALY =  FG_SARGA+ "B" + RESET
